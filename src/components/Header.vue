0dd7b093f1d1e544e5089bead3caa3a1416ed7a9
Michael Lance <michaelbraydenlance@gmail.com>
2024-05-21 20:57:39 -0600

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,68 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.webp" width="136" height="50" />
+    <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+
+  </header>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+header {
+  position:fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  width: auto;
+  display: flex;
+  margin: 0 5%;
+  background-color: black;
+}
+  nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-self: center;
+  }
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
